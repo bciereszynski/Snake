b0089f2fac48c6820d0841bf7fe7c790e57f53ce
Bartosz Ciereszyński <b.ciereszynski@wp.pl>
2022-10-09 23:36:35 +0200

    eating

---
+++ food_provider.py
@@ -0,0 +1,17 @@
+import random
+
+
+class FoodProvider:
+    def __init__(self, snake, y_limit, x_limit):
+        self.food = []
+        self.y_limit = y_limit
+        self.x_limit = x_limit
+        self.snake = snake
+
+    def generate(self):
+        while True:
+            y = random.randint(0, self.y_limit - 1)
+            x = random.randint(0, self.x_limit - 1)
+            if self.snake.segments.count([y, x]) == 0:
+                self.food.append([y, x])
+                break
--- snake.py
+++ snake.py
@@ -1,6 +1,8 @@
+from food_provider import FoodProvider
 
 
 class Snake:
+    # to moving
     vectors = {
         'Up': (0, -1),
         'Down': (0, 1),
@@ -8,11 +10,17 @@ class Snake:
         'Right': (1, 0),
     }
 
-    def __init__(self, y_limit, x_limit):
+    def __init__(self, y_limit, x_limit, fruit_count):
         self.y_limit = y_limit
         self.x_limit = x_limit
+
+        # TODO - initial configure
         self.segments = [[0, 0], [1, 0], [2, 0], [2, 1]]
         self.direction = 'Right'
+
+        self.food_provider = FoodProvider(self, y_limit, x_limit)
+        for x in range(fruit_count):
+            self.food_provider.generate()
 
     def __normalize_segments(self):
         for segment in self.segments:
@@ -24,14 +32,34 @@ class Snake:
                 segment[1] = 0
             elif segment[1] < 0:
                 segment[1] = self.x_limit - 1
+    #TODO Dodawanie na koncu
+    def try_eat(self, head, vector):
+        # eat fruit
+        if self.food_provider.food.count(head) != 0:
+            self.food_provider.food.remove(head)
+            self.segments.append(
+                [head[0] + vector[0], head[1] + vector[1]])
+            self.food_provider.generate()
+            return 0
+        # eat yourself
+        elif self.segments.count(head) != 1:
+            return 1
+        else:
+            return 0
 
     def move(self):
+        # gain vector coresponding to direction
         vector = self.vectors.get(self.direction, (0, 0))
+        # move last segment to front
         self.segments.pop(0)
-        first_segment = self.segments[-1]
+        head = self.segments[-1]
         self.segments.append(
-            [first_segment[0] + vector[0], first_segment[1] + vector[1]])
+            [head[0] + vector[0], head[1] + vector[1]])
         self.__normalize_segments()
+        # try eat - food(0) or yourself(1)
+        if self.try_eat(self.segments[-1], vector) == 1:
+            return 1
+        return 0
 
     def react(self, event):
         if event == 'KEY_UP' and self.direction != 'Down':
