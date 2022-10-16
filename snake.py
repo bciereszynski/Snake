69db030235589bf072fbe1ecdd6223bf38757fa0
Bartosz Ciereszyński <b.ciereszynski@wp.pl>
2022-10-16 18:22:32 +0200

    normalize opt

--- snake.py
+++ snake.py
@@ -17,28 +17,28 @@ class Snake:
         # TODO - initial configure
         self.segments = [[0, 0], [1, 0], [2, 0], [2, 1]]
         self.direction = 'Right'
+        self.has_eaten = False
 
         self.food_provider = FoodProvider(self, y_limit, x_limit)
         for x in range(fruit_count):
             self.food_provider.generate()
 
     def __normalize_segments(self):
-        for segment in self.segments:
-            if segment[0] >= self.y_limit:
-                segment[0] = 0
-            elif segment[0] < 0:
-                segment[0] = self.y_limit - 1
-            elif segment[1] >= self.x_limit:
-                segment[1] = 0
-            elif segment[1] < 0:
-                segment[1] = self.x_limit - 1
-    #TODO Dodawanie na koncu
+        segment = self.segments[-1]
+        if segment[0] >= self.y_limit:
+            segment[0] = 0
+        elif segment[0] < 0:
+            segment[0] = self.y_limit - 1
+        elif segment[1] >= self.x_limit:
+            segment[1] = 0
+        elif segment[1] < 0:
+            segment[1] = self.x_limit - 1
+
     def try_eat(self, head, vector):
         # eat fruit
         if self.food_provider.food.count(head) != 0:
             self.food_provider.food.remove(head)
-            self.segments.append(
-                [head[0] + vector[0], head[1] + vector[1]])
+            self.has_eaten = True
             self.food_provider.generate()
             return 0
         # eat yourself
@@ -51,7 +51,10 @@ class Snake:
         # gain vector coresponding to direction
         vector = self.vectors.get(self.direction, (0, 0))
         # move last segment to front
-        self.segments.pop(0)
+        if self.has_eaten == False:
+            self.segments.pop(0)
+        else:
+            self.has_eaten = False
         head = self.segments[-1]
         self.segments.append(
             [head[0] + vector[0], head[1] + vector[1]])
