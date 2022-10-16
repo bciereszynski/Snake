5a2750a3ec00a4608d2d2fb4ba6b55b7b889fa98
Bartosz Ciereszyński <b.ciereszynski@wp.pl>
2022-10-16 18:35:47 +0200

    refacttor

--- game.py
+++ game.py
@@ -1,6 +1,4 @@
 
-import pygame
-import keyboard
 
 from snake import Snake
 
@@ -10,10 +8,6 @@ class Game:
         self.fruit_count = 2
         self.window = window
         self.snake = Snake(window.height, window.width, self.fruit_count)
-
-        pygame.init()
-        self.fps = 10
-        self.fpsClock = pygame.time.Clock()
 
     def draw_food(self, snake):
         for fruit in snake.food_provider.food:
@@ -25,16 +19,6 @@ class Game:
 
     # curses have CURSED orientation
 
-    def translate_event(self):
-        if keyboard.is_pressed("up arrow"):
-            return 'KEY_LEFT'
-        if keyboard.is_pressed("down arrow"):
-            return 'KEY_RIGHT'
-        if keyboard.is_pressed("left arrow"):
-            return 'KEY_UP'
-        if keyboard.is_pressed("right arrow"):
-            return 'KEY_DOWN'
-
     def play(self):
         while True:
             if self.snake.move() == 1:
@@ -44,7 +28,7 @@ class Game:
             self.draw_snake(self.snake)
             self.draw_food(self.snake)
             self.window.refresh()
-            self.snake.react(self.translate_event())
-            self.fpsClock.tick(self.fps)
+            self.snake.react(self.window.translate_event())
+            self.window.fpsTick()
 
         self.window.clear()
